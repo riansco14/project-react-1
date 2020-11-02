bb456c803236473ac858422c596618fece65b8f2
riansco14 <rian.rabelo@ufrpe.br>
2020-11-02 12:27:51 -0300

    Configs Fix

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -2,4 +2,15 @@ import { Router } from 'express'
 
 const routes = Router()
 
+routes.post('/', (req, res) => {
+	const { name, email } = req.body
+
+	const user = {
+		name,
+		email,
+	}
+
+	return res.json({ message: user })
+})
+
 export default routes
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,8 +3,10 @@ import routes from './routes'
 
 const app = express()
 
+app.use(express.json())
 app.use(routes)
 
 app.listen(3333, () => {
 	console.log('Server iniciado in 3333')
+	const a = { asdsa: 'asdsa' }
 })
